98bf1612aa51aa30f54306b4df5b433cb869a082
Karlo Poljanec <kp49062@fer.hr>
2018-12-18 18:31:17 +0100

    Init

---
+++ ProjektMAS.sdk/mas_camera/Debug/src/main.d
@@ -0,0 +1,52 @@
+src/main.o src/main.o: ../src/main.c \
+ ../../mas_camera_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../mas_camera_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../mas_camera_bsp/ps7_cortexa9_0/include/xiicps.h \
+ ../../mas_camera_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../mas_camera_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../mas_camera_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../mas_camera_bsp/ps7_cortexa9_0/include/xiicps_hw.h \
+ ../../mas_camera_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../mas_camera_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../mas_camera_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../mas_camera_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../mas_camera_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../mas_camera_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../mas_camera_bsp/ps7_cortexa9_0/include/xplatform_info.h \
+ ../../mas_camera_bsp/ps7_cortexa9_0/include/xgpio.h \
+ ../../mas_camera_bsp/ps7_cortexa9_0/include/xgpio_l.h \
+ ../../mas_camera_bsp/ps7_cortexa9_0/include/xil_printf.h
+
+../../mas_camera_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../mas_camera_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../mas_camera_bsp/ps7_cortexa9_0/include/xiicps.h:
+
+../../mas_camera_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../mas_camera_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../mas_camera_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../mas_camera_bsp/ps7_cortexa9_0/include/xiicps_hw.h:
+
+../../mas_camera_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../mas_camera_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../mas_camera_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../mas_camera_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../mas_camera_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../mas_camera_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../mas_camera_bsp/ps7_cortexa9_0/include/xplatform_info.h:
+
+../../mas_camera_bsp/ps7_cortexa9_0/include/xgpio.h:
+
+../../mas_camera_bsp/ps7_cortexa9_0/include/xgpio_l.h:
+
+../../mas_camera_bsp/ps7_cortexa9_0/include/xil_printf.h:
---
+++ ProjektMAS.sdk/mas_camera_bsp_xgpio_example_1/Debug/src/xgpio_example.d
@@ -0,0 +1,43 @@
+src/xgpio_example.o src/xgpio_example.o: ../src/xgpio_example.c \
+ ../../mas_camera_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../mas_camera_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../mas_camera_bsp/ps7_cortexa9_0/include/xgpio.h \
+ ../../mas_camera_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../mas_camera_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../mas_camera_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../mas_camera_bsp/ps7_cortexa9_0/include/xgpio_l.h \
+ ../../mas_camera_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../mas_camera_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../mas_camera_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../mas_camera_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../mas_camera_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../mas_camera_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../mas_camera_bsp/ps7_cortexa9_0/include/xil_printf.h
+
+../../mas_camera_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../mas_camera_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../mas_camera_bsp/ps7_cortexa9_0/include/xgpio.h:
+
+../../mas_camera_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../mas_camera_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../mas_camera_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../mas_camera_bsp/ps7_cortexa9_0/include/xgpio_l.h:
+
+../../mas_camera_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../mas_camera_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../mas_camera_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../mas_camera_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../mas_camera_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../mas_camera_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../mas_camera_bsp/ps7_cortexa9_0/include/xil_printf.h:
